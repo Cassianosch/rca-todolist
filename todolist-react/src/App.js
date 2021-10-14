e3d80279854a7c4f9e9a469738acea37899008b7
Cassiano S <cassiano@scheidemantel.com.br>
2021-10-14 20:45:51 -0300

    feat: now edit and check tool is working add moookup

--- todolist-react/src/App.js
+++ todolist-react/src/App.js
@@ -2,39 +2,81 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-	const [list, setList] = useState(['a', 'b']);
-	const [inputValue, setInputValue] = useState('');
+	const [list, setList] = useState([
+		{
+			id: 1,
+			task: 'Lavar o carro',
+			checked: false
+		},
+		{
+			id: 2,
+			task: 'Ver o sol',
+			checked: true
+		},
+		{
+			id: 3,
+			task: 'Limpar monitor',
+			checked: false
+		},
+		{
+			id: 4,
+			task: 'Trocar a lampada',
+			checked: false
+		}
+	]);
+	const [inputValue, setInputValue] = useState({});
+	const [editing, setEditing] = useState(false);
 
-	const handleAddItem = () => {
+	const handleAddEditItem = () => {
+		if (!!editing) {
+			const allItems = list.map(item => {
+				if (item.id === editing) {
+					inputValue.id = editing;
+					return inputValue;
+				}
+				return item;
+			});
+			setList(allItems);
+			setEditing(false);
+			return;
+		}
+
 		if (inputValue !== '') {
 			setList(oldA => [...oldA, inputValue])
 		}
 		setInputValue('');
 	}
-	const handleEditInput = (value) => {
-		//TODO: Edit
-		setList(oldValues => oldValues.filter(el => el !== value))
+	const handleClickItem = (el) => {
+		setEditing(el.id);
+		setInputValue(el);
 	}
-
-	const handleDeleteInput = (value) => {
-		setList(oldValues => oldValues.filter(el => el !== value))
+	const handleCheckDoneItem = (idElement) => {
+		const allItems = list.map(item => {
+			if (item.id === idElement) item.checked = !item.checked;
+			return item;
+		});
+		setList(allItems);
+	}
+	const handleDeleteInput = (idElement) => {
+		setList(oldValues => oldValues.filter(el => el.id !== idElement));
 	}
 	return (
 		<div className="App">
-			<input onChange={e => setInputValue(e.target.value)} value={inputValue} />
-			<button onClick={handleAddItem}>Add</button>
+			<input onChange={e => setInputValue({ id: Math.floor(Math.random() * (100000)), task: e.target.value })} value={inputValue?.task || ''} />
+			<button onClick={handleAddEditItem}>{!!editing ? 'Update' : 'Add'}</button>
 			{
 				list && (
 					<ul>
 						{list.map((el, i) => (
-							<>
-								<li onClick={() => handleEditInput(el)} key={i}>
-									<u>
-										{el}
+							<div key={i}>
+								<li onClick={() => handleClickItem(el)}>
+									<u className={el.checked ? 'line-through' : null}>
+										{el.task}
 									</u>
 								</li>
-								<button onClick={() => handleDeleteInput(el)}>Delete</button>
-							</>
+								<button onClick={() => handleCheckDoneItem(el.id)}>Done</button>
+								<button onClick={() => handleDeleteInput(el.id)}>Delete</button>
+							</div>
 						))}
 					</ul>
 				)
